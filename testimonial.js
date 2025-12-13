819923df19d97774f4b1fe3a98cf0b2e9151ee3e
quantumrootsystem-jpg <quantumrootsystem@gmail.com>
2025-12-13 09:48:38 +0530

    Add files via upload

--- testimonial.js
+++ testimonial.js
@@ -4,11 +4,14 @@ rotateTestimonials();
 function rotateTestimonials() {
   let testimonials = document.getElementsByClassName("testimonial-box");
 
-  for (let i = 0; i < testimonials.length; i++)
+  for (let i = 0; i < testimonials.length; i++) {
     testimonials[i].style.display = "none";
+  }
 
   tIndex++;
-  if (tIndex > testimonials.length) tIndex = 1;
+  if (tIndex > testimonials.length) {
+    tIndex = 1;
+  }
 
   testimonials[tIndex - 1].style.display = "block";
 
